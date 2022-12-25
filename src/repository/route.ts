67f40da69816257bf90ed6e77c173591960ae4dd
HalidOdat <halidodat@gmail.com>
2022-12-25 21:29:33 +0000

    Commit: (2618978) Point frontend to server url

--- src/repository/route.ts
+++ src/repository/route.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import httpClient from '../infrastructure/http-client';
 import {Point} from "ol/geom";
 
-const ServerLocation = 'https://connect-navigation-server.onrender.com';
+const ServerLocation = 'https://connect-production-91d2.up.railway.app';
 
 @Injectable()
 export class RouteRepository {
