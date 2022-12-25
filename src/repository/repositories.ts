806ccb85160e098417cdf88d041ffc09c4b2165c
HalidOdat <halidodat@gmail.com>
2022-12-25 11:05:28 +0000

    Commit: (9697dd1) Merge pull request #26 from connect-navigation/feature/github-action-ci
    Split frontend and backend to separate branches

---
+++ src/repository/repositories.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import httpClient from '../infrastructure/http-client';
+
+const ServerLocation = 'http://localhost:8080';
+
+@Injectable()
+export class CoffeeShopRepository {
+  async all() {
+    const resp = await fetch(ServerLocation + '/api/coffee-shops/all');
+    const data = await resp.json();
+    return data;
+  }
+}
+
+@Injectable()
+export class GasStationRepository {
+  async all() {
+    const resp = await fetch(ServerLocation + '/api/gas-stations/all');
+    const data = await resp.json();
+    return data;
+  }
+}
+
+@Injectable()
+export class HotelRepository {
+  async all() {
+    const resp = await fetch(ServerLocation + '/api/hotels/all');
+    const data = await resp.json();
+    return data;
+  }
+}
+
+@Injectable()
+export class SuperMarketRepository {
+  async all() {
+    const resp = await fetch(ServerLocation + '/api/super-markets/all');
+    const data = await resp.json();
+    return data;
+  }
+}
+
+@Injectable()
+export class RestaurantRepository {
+  async all() {
+    const resp = await fetch(ServerLocation + '/api/restaurants/all');
+    const data = await resp.json();
+    return data;
+  }
+}
+
---
+++ src/repository/route.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import httpClient from '../infrastructure/http-client';
+import {Point} from "ol/geom";
+
+const ServerLocation = 'http://localhost:8080';
+
+@Injectable()
+export class RouteRepository {
+  async route(fromLat: number, fromLon: number, toLat: number, toLon: number) {
+    const resp = await fetch(ServerLocation + `/api/route/${fromLat}/${fromLon}/${toLat}/${toLon}`);
+    const data = await resp.json();
+    return data;
+  }
+}
