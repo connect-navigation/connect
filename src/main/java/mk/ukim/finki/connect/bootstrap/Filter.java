0b7de806b10aa549487b1152ccd57f769b1a1b5c
Halid Odat <halidodat@gmail.com>
2022-12-25 11:58:46 +0100

    empty

--- src/main/java/mk/ukim/finki/connect/bootstrap/Filter.java
+++
@@ -1,5 +0,0 @@
-package mk.ukim.finki.connect.bootstrap;
-
-public interface Filter {
-    public Object run(Object input);
-}
--- src/main/java/mk/ukim/finki/connect/service/HotelService.java
+++
@@ -1,9 +0,0 @@
-package mk.ukim.finki.connect.service;
-
-import mk.ukim.finki.connect.model.Hotel;
-
-import java.util.List;
-
-public interface HotelService {
-    List<Hotel> findAll();
-}
